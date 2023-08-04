8d9889dcb356c7e603e5217497268e0693cb35b0
kwonskdev <91136723+kwonskdev@users.noreply.github.com>
2023-08-04 06:48:41 +0000

    <feat>: Add batch_prediction and service
    - batch_prediction.py
    - service.py

---
+++ batch_prediction.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import warnings
+from datetime import datetime
+
+import joblib
+import numpy as np
+import pandas as pd
+
+from src.common.constants import (
+    ARTIFACT_PATH,
+    DATA_PATH,
+    LOG_FILEPATH,
+    PREDICTION_PATH,
+)
+from src.common.logger import handle_exception, set_logger
+
+logger = set_logger(os.path.join(LOG_FILEPATH, "logs.log"))
+sys.excepthook = handle_exception
+warnings.filterwarnings(action="ignore")
+
+if __name__ == "__main__":
+    DATE = datetime.now().strftime("%Y%m%d")
+    logger.info("Loading the test data...")
+    test = pd.read_csv(os.path.join(DATA_PATH, "bike_sharing_test.csv"))
+
+    logger.info("Loading a pre-trained pipeline")
+    model = joblib.load(os.path.join(ARTIFACT_PATH, "model.pkl"))
+
+    X = test.drop(["datetime", "count"], axis=1, inplace=False)
+    id_ = test["datetime"].to_numpy()
+
+    logger.info("Saving a feature data for the test data...")
+    model["preprocess"].transform(X=X).to_csv(
+        os.path.join(DATA_PATH, "storage", "bike_sharing_test_features.csv"),
+        index=False,
+    )
+
+    pred_df = pd.DataFrame({"datetime": id_, "count": model.predict(X)})
+    logger.info(f"Batch prediction for {len(pred_df)} times is created.")
+
+    save_path = os.path.join(PREDICTION_PATH, f"{DATE}_count_prediction.csv")
+    pred_df.to_csv(save_path, index=False)
+
+    logger.info(f"Prediction can be found in the following path: \n{save_path}")
---
+++ service.py
@@ -0,0 +1,30 @@
+import bentoml
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from bentoml.io import JSON, NumpyNdarray
+from pydantic import BaseModel
+
+
+class Features(BaseModel):
+    season: int
+    holiday: int
+    workingday: int
+    weather: int
+    temp: float
+    atemp: float
+    humidity: int
+    windspeed: float
+
+
+bento_model = bentoml.sklearn.get("bike_sharing:latest")
+model_runner = bento_model.to_runner()
+
+svc = bentoml.Service("bike_sharing_regressor", runners=[model_runner])
+
+
+@svc.api(input=JSON(pydantic_model=Features), output=NumpyNdarray())
+async def predict(input_data: Features) -> npt.NDArray:
+    input_df = pd.DataFrame([input_data.dict()])
+    log_pred = await model_runner.predict.async_run(input_df)
+    return log_pred
